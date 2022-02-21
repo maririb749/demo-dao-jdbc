2f20a7e9fb105edd132f4d46887cc75b413d5541
Mariana Santos <maririb51@gmail.com>
2022-02-21 15:54:36 +0000

    SellerDaoJBDC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,9 +16,10 @@ public class Program {
 		Seller seller = new Seller(21, "Bob", "bob@hmail.com", new Date(), 3000.00, obj);
 		System.out.println(obj);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao()
+		
 		System.out.println(seller);
 		
-
 	}
 
 }
